07783bd779a9ccdd07ab4d1f04c4daea8bcd92e3
GalaAldebara <lalasipo20@gmail.com>
2025-10-22 08:43:01 +0700

    Bismillah

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,6 +19,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // ✅ Force HTTPS di production
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }
